64b3d03bbf5db6a1d128cd9c2e7bf504b287906b
sayan <sayan@SAYAN>
2025-09-05 15:48:20 +0530

    reverse string program added

---
+++ src/com/mainapp/Reverse_A_String.java
@@ -0,0 +1,33 @@
+package com.mainapp;
+
+public class Reverse_A_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      reverse("sayan");
+	}
+	public static void reverse(String s) {
+		String rev="";	
+		//Way 1: Using +(String concatination)operator
+		
+//		for(int i=s.length()-1;i>=0;i--) {
+//			rev=rev+s.charAt(i);
+//		}
+//		System.out.println(rev);
+		
+		//Way 2: Using char Array
+//		
+//		char a[]=s.toCharArray();
+//		for(int i=a.length-1;i>=0;i--) {
+//			rev=rev+a[i];
+//		}
+//		System.out.println(rev);
+		
+		//Way 3: Using StringBuffer class
+		
+		StringBuffer buffer = new StringBuffer(s);
+		System.out.println(buffer.reverse());
+	}
+	
+
+}
