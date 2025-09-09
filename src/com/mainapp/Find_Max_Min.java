60c108e649a7621368781f60fe45ef3309524fe9
Sayan Metya <metyasayan3@gmail.com>
2025-09-10 00:07:24 +0530

    Create Find_Max_Min.java

---
+++ src/com/mainapp/Find_Max_Min.java
@@ -0,0 +1,29 @@
+package com.mainapp;
+
+public class Find_Max_Min {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {3,4,54,64,2};
+Findd findd = new Findd(arr);
+	}
+
+}
+class Findd{
+	public Findd(int a[]) {
+//		int max=a[0];
+//		for(int i=1;i<a.length;i++) {
+//			if(a[i]>max) {
+//				max=a[i];
+//			}
+//		}
+//		System.out.println("the max number is="+max);
+		//int min=a[0];
+//		for(int i=1;i<a.length;i++) {
+//			if(a[i]<min) {
+//				min=a[i];
+//			}
+//		}
+//		System.out.println("the max number is="+min;
+	}
+}
